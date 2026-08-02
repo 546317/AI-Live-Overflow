b62886c421cb8e8be0140fd8986c336bb1351702
莉莉 <qwe54631754@outlook.com>
2026-08-02 18:45:41 +0800

    Update PetActivity.kt

--- app/src/main/java/com/ly/pet/PetActivity.kt
+++ app/src/main/java/com/ly/pet/PetActivity.kt
@@ -7,8 +7,6 @@ import android.widget.Toast
 class PetActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
-        // 最简单的测试：弹个提示
-        Toast.makeText(this, "启动了", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "桌宠启动了", Toast.LENGTH_LONG).show()
     }
 }
