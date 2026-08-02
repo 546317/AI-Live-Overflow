07a167d72b02aa08f5cb39e473727722857bac87
莉莉 <qwe54631754@outlook.com>
2026-08-02 12:13:42 +0800

    Update PetActivity.kt

--- app/src/main/java/com/ly/pet/PetActivity.kt
+++ app/src/main/java/com/ly/pet/PetActivity.kt
@@ -1,13 +1,33 @@
 package com.ly.pet
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.provider.Settings
 
-class PetActivity : AppCompatActivity() {
+class PetActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startService(Intent(this, OverlayService::class.java))
-        finish()
+        if (!Settings.canDrawOverlays(this)) {
+            val intent = Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName")
+            )
+            startActivityForResult(intent, 1001)
+        } else {
+            startService(Intent(this, OverlayService::class.java))
+            finish()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1001) {
+            if (Settings.canDrawOverlays(this)) {
+                startService(Intent(this, OverlayService::class.java))
+            }
+            finish()
+        }
     }
 }
